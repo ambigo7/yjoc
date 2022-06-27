11c76cfd874e3f5de58382881c644573f2a0a869
Ambigo7 <dendirizkapoetra@gmail.com>
2022-06-27 17:42:13 +0700

    Bug Send Mail

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,8 @@
 <?php
+
+ini_set( 'display_errors', 1 );   
+error_reporting( E_ALL ); 
+
 // Check for empty fields
 if(empty($_POST['name'])  		||
    empty($_POST['email']) 		||
@@ -18,13 +22,27 @@ $message = $_POST['message'];
 // Create the email and send the message 'admin@yjoc.web.id'
 $to = 'admin@yjoc.web.id'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website YJOC Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
-$headers .= "Reply-To: $email_address";	
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\n
+Name: $name\n\n
+Email: $email_address\n\n
+Phone: $phone\n\n
+Message:\n$message";
+
+$headers = 'From: noreply@yjoc.web.id' . "\r\n" . 
+    'Reply-To: $email_address' . "\r\n" . 
+    'X-Mailer: PHP/' . phpversion(); 
+    
+//$headers = "From: noreply@yjoc.web.id\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+//$headers .= "Reply-To: $email_address";	
 
 /*ini_set("SMTP","mail.yjoc.web.id");
 ini_set("smtp_port","993");*/
 
 mail($to,$email_subject,$email_body,$headers);
-return true;			
+if(!$mail) {
+    echo “Mailer Error: ”;
+} else {
+    echo “Message has been sent successfully ”;
+    return true;
+}
 ?>
